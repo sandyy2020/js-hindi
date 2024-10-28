77c3719de1fa998d65631bfb2eec6ed843353e20
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-10-28 12:40:08 +0000

    basic done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId=144553
 let accountEmail='sa@gmail.com'
 var accountPassword="654321"
 accountCity="delhi"
+let accountState
 //accountId=2
 accountEmail="sandy@gmail.com"
 accountPassword="12345"
@@ -10,4 +11,8 @@ console.log(accountId);
 console.log(accountEmail);
 console.log(accountPassword);
 console.log(accountCity);
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+Prefer not to use var because of issue in block
+and functional scope
+*/
