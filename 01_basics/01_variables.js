a6adc3863ffd8c3e73e2466b79c6f694593a5635
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-10-28 12:17:42 +0000

    basic done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail='sa@gmail.com'
+var accountPassword="654321"
+accountCity="delhi"
+//accountId=2
+accountEmail="sandy@gmail.com"
+accountPassword="12345"
+accountCity='delhi'
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
