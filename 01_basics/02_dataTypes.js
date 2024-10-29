62819cb39dc3802439c6e2b58d30d3acb36402e4
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-10-29 07:07:58 +0000

    datatype done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"Use strict" //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+console.log("sandy")
+
+let name="sandy"
+let age=27
+let isLoggedIn= false
+let state= null
+let phone
+
+//number=>2 to power 53
+//bigint
+//string=>""(prefered) or ''
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object
+
+console.log(typeof undefined) //ouput will be undefined
+console.log(typeof null) //output will be object
