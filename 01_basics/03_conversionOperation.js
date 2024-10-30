c3af2ac182e497bab697d4434139ad414fe1eaa9
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-10-30 07:56:51 +0000

    conversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,3 +23,35 @@ let someNumber=33
 let stringNumber= String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//*******operation**********
+let value = 3
+let negValue= -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(5%3)
+
+let str1="hello"
+let str2=" sandy"
+let str3=str1 + str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log((3+4)*5%3)
+console.log(true)
+console.log(+true)
+
+let gameCounter=100
+//gameCounter++
+++gameCounter
+console.log(gameCounter)
+
+//link to study
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
