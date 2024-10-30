d9435e19525b3d890623fdb4e2e5421822b6fae2
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-10-30 07:33:06 +0000

    conversion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+
+let scores = "33abc"
+let valueInNumber = Number(scores)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn="sandy"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true; 0=>false
+//""=>false
+//"sandy"=>true
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
