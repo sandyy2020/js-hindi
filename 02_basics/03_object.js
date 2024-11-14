3c411e9537536f0445d5d9b443106202d1789063
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-14 07:32:06 +0000

    object1 done

---
+++ 02_basics/03_object.js
@@ -0,0 +1,28 @@
+//singleton
+//object.create
+
+//object Literals
+
+const mySym=Symbol("key1") //key1 is declared as Symbol
+
+const JsUser={
+    name: "sandy",
+    'full name':'sandy s',
+    [mySym]:"mykey1",
+    email:"sandy@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays: ['Monday','Saturday']
+}
+
+console.log(JsUser.email)
+//console.log(JsUser.full name) // full name will never be accessed in this way
+//or
+console.log(JsUser['email'])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])
+
+JsUser.email='chat@gmail.com'
+console.log(JsUser.email) //use to freeze the object. Now value of email will not change further
+Object.freeze(JsUser)
+JsUser.email='test@gmail.com'
+console.log(JsUser)
