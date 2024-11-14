e78c568814c8974f4d4c5dc1cabf11c7dfb94d34
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-14 07:53:46 +0000

    object1 done

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -3,7 +3,7 @@
 
 //object Literals
 
-/*const mySym=Symbol("key1") //key1 is declared as Symbol
+const mySym=Symbol("key1") //key1 is declared as Symbol
 
 const JsUser={
     name: "sandy",
@@ -19,15 +19,19 @@ console.log(JsUser.email)
 //or
 console.log(JsUser['email'])
 console.log(JsUser["full name"])
-console.log(JsUser[mySym])
+console.log(JsUser[mySym]) //used to access mySym as Symbol
 
 JsUser.email='chat@gmail.com'
 console.log(JsUser.email) //use to freeze the object. Now value of email will not change further
 //Object.freeze(JsUser)
 JsUser.email='test@gmail.com'
-console.log(JsUser) */
+//console.log(JsUser)
 
 JsUser.greeting=function(){
     console.log('Hello Js User')
 }
+JsUser.greeting2=function(){
+    console.log(`Hello JS user, ${this.name}`)
+}
 console.log(JsUser.greeting())
+console.log(JsUser.greeting2())
