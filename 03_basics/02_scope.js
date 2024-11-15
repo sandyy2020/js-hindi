771b3ad5cbacc9f073dfa109c22a5deeaa158799
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-15 05:48:37 +0000

    scope done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,7 +8,7 @@
 let a=300
 {
     let a=10
-    const b=20    //block scope
+    const b=20    //local scope
     //var c=30
     console.log('INNER:',a)
 }
@@ -16,3 +16,38 @@ let a=300
 console.log(a)
 //console.log(b)
 //console.log(c)
+
+function one(){
+    const username="sandy"
+
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    two()
+}
+one()
+
+if(true){
+    const username= 'sandyy'
+    if(username == 'sandyy'){
+        const website="youtube"
+        console.log(username + website)
+    }
+    //console.log(website)
+}
+//console.log(username)
+
+///************interesting****/
+
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+//addTwo(5) //addTwo cannot be declare first 
+const addTwo=function(num){
+    return num+2
+}
