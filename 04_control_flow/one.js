fcbe6b98d14deb16d3aaccf1ef31a9c6cdf4a48e
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-16 12:31:15 +0000

    one done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -12,5 +12,21 @@ if(temp<40){
     console.log('Temp is too hot')
 }
 
-
 // <, >, <=, >=, ==, !=, === comparison operator
+
+const score=300
+if(score>100){
+    const power="fly"
+    console.log(`User power: ${power}`)
+}
+
+const balance=1000
+if(balance>500) console.log("test") //it is called implict scope which is written in one line only
+
+if(balance<500){
+    console.log("less than 500")
+}else if(balance<750){
+    console.log("less than 750")
+}else{
+    console.log("less than 1000")
+}
