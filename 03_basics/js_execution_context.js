75f51daac4f2b059fd015496b9018c5452c205a6
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-16 10:44:11 +0000

    jscontext

---
+++ 03_basics/js_execution_context.js
@@ -0,0 +1,8 @@
+ // Javascript Execution Context
+  // 1.Global Execution Context (refer by variable this)
+  // 2.Function Execution Context
+  // 3.Eval Execution Context
+
+// JS executes in two phases
+// 1. Memory Creation Phase (Creation Phase)
+// 2. Execution Phase
