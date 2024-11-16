cfac89c5db56cacecd3aa4e56e223f2fe7993e03
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-16 12:16:25 +0000

    one done

---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+//if
+
+const isUserLoggedIn=true
+if(2 != 3){
+    console.log("executed")
+}
+
+const temp=35
+if(temp<40){
+    console.log('Temp is less hot')
+}else{
+    console.log('Temp is too hot')
+}
+
+
+// <, >, <=, >=, ==, !=, === comparison operator
