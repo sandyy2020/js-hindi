fd222a13299b60cec70c26e81acc112f709b8255
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-16 06:49:13 +0000

    iife done

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -5,13 +5,17 @@
 // }
 // chai()
 
-(function chai(){
+(function chai(){  //named IIFE
     console.log(`DB CONNECTED`)
 })();
 
-( ()=>{
+( ()=>{  //Unnamed IIFE
  console.log(`DB CONNECTED TWO`)
-})()
+})();
+
+((name)=>{
+    console.log(`DB CONNECTED TWO ${name}`)
+})('sandyy') //parameter passed
 
 
 //()(); for iffe ; is also compulsory
