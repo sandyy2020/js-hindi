748b3ff43be8bc58f064c8e8d28e1be445edd658
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-16 12:01:24 +0000

    jscontext done

--- 03_basics/js_execution_context.js
+++ 03_basics/js_execution_context.js
@@ -6,3 +6,24 @@
 // JS executes in two phases
 // 1. Memory Creation Phase (Creation Phase)
 // 2. Execution Phase
+
+let val1=10
+let val2=5
+function addNum(num1,num2){
+    let total=num1+num2
+    return total
+}
+let result1=addNum(val1,val2)
+let result2=addNum(10,2)
+
+1.Global Execuction(Global environment)->this
+2.Memory Phase
+val1=>undefined
+val2=>undefined
+addNum=>definition
+result1=>undefined
+result2=>undefined
+3.Execution Phase
+val1<=10
+val2<=5
+addNum=>new variable environment+ execution thread
