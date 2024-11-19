c848c827b0814f3990f37359db63daf4f52024eb
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-19 06:04:57 +0000

    switch done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+// switch(key){
+//     case value:
+//         break
+//     default:
+//         break
+// }
+
+const month=2
+
+switch(month){
+    case 1:
+        console.log('january');
+        break
+    case 2:
+        console.log('feb');
+        break
+    case 3:
+        console.log('march');
+        break
+    case 4:
+        console.log('april');
+        break
+    default:
+        console.log('default case match')
+        break
+        
+}
+const months="april"
+
+switch(months){
+    case "jan":
+        console.log('january');
+        break
+    case "feb":
+        console.log('feb');
+        break
+    case "march":
+        console.log('march');
+        break
+    case "april":
+        console.log('april');
+        break
+    default:
+        console.log('default case match')
+        break
+        
+}
