f7f5e6abd7e9651688e9ceaab7483311888d5de6
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2024-11-19 05:53:12 +0000

    one done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -30,3 +30,16 @@ if(balance<500){
 }else{
     console.log("less than 1000")
 }
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+
+if(userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course")
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User Logged In")
+}
