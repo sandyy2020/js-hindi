3e3d9e016481bacac31443b283ccaf77967042b5
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2025-01-30 12:33:54 +0000

    do-while iteration

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,3 +1,29 @@
 // for loop
 
-console.log('Hi how are you')
+//console.log('Hi how are you')
+
+let index=0
+while(index<=10){
+    //console.log(`value of index is ${index}`);
+    index=index+2
+}
+
+let myArray=['flash',"batman",'superman']
+let arr=0
+while(arr<myArray.length){
+    //console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+}
+
+let score1=1
+do{
+    //console.log(`score is ${score1}`);
+    score1++
+}while(score1<=10)
+
+    let score=11
+    do{
+        console.log(`score is ${score}`);
+        score++
+    }while(score<=10)
+// first execute for one time then check the condition
