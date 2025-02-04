b160c0302270b38751230f33f00bc6590f6b41d5
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2025-02-04 06:40:31 +0000

    iife example

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -20,3 +20,15 @@
 
 //()(); for iffe ; is also compulsory
 //iffe is a function which is used for immediately execution.And the main function of iife is to solve the problem of global scope pollution.
+
+const numbers=[1,2,3,4,5,6]; // Declares an array named 'numbers' with six elements.
+(function abc(...number1){ // Defines an Immediately Invoked Function Expression (IIFE) named 'abc' with a rest parameter 'number1'.
+    console.log(`print the number ${number1}`) // Logs a string to the console, interpolating the value of 'number1'.
+})(numbers); // Calls the IIFE immediately, spreading the 'numbers' array as arguments to the function.
+
+(function anything(...number1){
+    console.log(`print the number ${number1}`);
+})(1,2,3,4,5);
+
+//(...)Rest Parameter: Used in function definitions to collect all arguments into an array.
+//(...)Spread Operator: Used to spread elements of an array or properties of an object into individual elements or key-value pairs.
