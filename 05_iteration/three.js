b6f1c6197a9b058782c7a87e4685c4aead0b6cc5
Sandeep Singh <76576905+sandyy2020@users.noreply.github.com>
2025-02-04 06:41:30 +0000

    for of

---
+++ 05_iteration/three.js
@@ -0,0 +1,45 @@
+//Array specific loop
+
+// for of
+
+//["",'',""]
+//[{},{},{}]
+
+const arr=[1,2,3,4,5]
+
+for(const num of arr){
+    console.log(num);
+}
+
+const greetings="Hello world!"
+for(const greet of greetings){
+    console.log(`Each Char is ${greet}`);
+}
+
+//Maps
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA','United State of America')
+map.set('Fr',"France")
+console.log(map);
+
+for(const key of map){
+    console.log(key);
+}
+
+//map is used for unique value, duplicate value is not shown 
+//the result of map is shown in same order as it is written
+
+for(const [key,value] of map){
+    console.log(key,':-',value);
+}
+
+const myobject={
+    'game1':'NFS',
+    'game2':'spiderman'
+}
+
+for(const[key,value] of myobject){
+    console.log(key,':-',value); //myobject is not iterable so it is not used directly
+}
